add85b739807250261f5e4241cd7d019b62647a8
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-05-04 00:50:21 -0400

    updated to add reaction routes

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ app.use(express.urlencoded({ extended: true }));
 
 const userRoutes = require('./routes/api/user-routes');
 const thoughtRoutes = require('./routes/api/thought-routes');
+const reactionRoutes = require('./routes/api/reaction-routes');
 
 app.use('/api/users', userRoutes);
 app.use('/api/thoughts', thoughtRoutes);
+app.use('/api/reactions', reactionRoutes);
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
   useFindAndModify: false,
