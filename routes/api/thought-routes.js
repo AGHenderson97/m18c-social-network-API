37d73e8b6e09d2c43796c405dae904256684c755
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-05-03 23:43:46 -0400

    fixing errors

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,4 +1,4 @@
-const router = require('express').Router(thoughtController);
+const thoughtController = require('../../controllers/thought-controller');
 const {
   getAllThoughts,
   getThoughtById,
