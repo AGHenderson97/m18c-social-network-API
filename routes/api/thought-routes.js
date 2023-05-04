19438a8dab6d3554e8d9643dcc236ec0a79c41ef
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-05-03 23:47:29 -0400

    added api

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,10 +1,27 @@
-const thoughtController = require('../../controllers/thought-controller');
+const router = require('express').Router();
 const {
   getAllThoughts,
   getThoughtById,
   createThought,
   updateThought,
   deleteThought,
-  createReaction,
-  deleteReaction,
-} = require('../../server');
+  addReaction,
+  removeReaction,
+} = require('../../controllers/thought-controller');
+
+// /api/thoughts
+router.route('/').get(getAllThoughts);
+
+// /api/thoughts/:id
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:userId
+router.route('/:userId').post(createThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
